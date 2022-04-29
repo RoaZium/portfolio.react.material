01b5137c6069b41269c7feb1019c7a6f36309b2e
황진욱 <40532902+RoaZium@users.noreply.github.com>
2022-04-29 14:57:35 +0900

    작업중 06

--- src/examples/Sidenav/SidenavCollapse.js
+++ src/examples/Sidenav/SidenavCollapse.js
@@ -1,18 +1,3 @@
-/**
-=========================================================
-* Material Dashboard 2 React - v2.1.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/material-dashboard-react
-* Copyright 2022 Creative Tim (https://www.creative-tim.com)
-
-Coded by www.creative-tim.com
-
- =========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
 // prop-types is a library for typechecking of props.
 import PropTypes from "prop-types";
 
@@ -21,9 +6,6 @@ import ListItem from "@mui/material/ListItem";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import Icon from "@mui/material/Icon";
-
-// Material Dashboard 2 React components
-import MDBox from "components/MDBox";
 
 // Custom styles for the SidenavCollapse
 import {
@@ -35,14 +17,15 @@ import {
 
 // Material Dashboard 2 React context
 import { useMaterialUIController } from "context";
+import { Box } from "@mui/material";
 
 function SidenavCollapse({ icon, name, active, ...rest }) {
   const [controller] = useMaterialUIController();
   const { miniSidenav, transparentSidenav, whiteSidenav, darkMode, sidenavColor } = controller;
 
   return (
-    <ListItem component="li">
-      <MDBox
+    <ListItem>
+      <Box
         {...rest}
         sx={(theme) =>
           collapseItem(theme, {
@@ -77,7 +60,7 @@ function SidenavCollapse({ icon, name, active, ...rest }) {
             })
           }
         />
-      </MDBox>
+      </Box>
     </ListItem>
   );
 }
